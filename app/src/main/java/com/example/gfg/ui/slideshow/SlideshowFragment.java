a93526a702cb19331f265ded6de28c5bae47696f
Soumyadeep Dawn <soumyadeepdawn2002@gmail.com>
2023-03-27 17:32:39 +0530

    I made the android view of the gfg website to use it into our android devices easily

---
+++ app/src/main/java/com/example/gfg/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,72 @@
+package com.example.gfg.ui.slideshow;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.gfg.databinding.FragmentSlideshowBinding;
+
+public class SlideshowFragment extends Fragment {
+
+    private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SlideshowViewModel slideshowViewModel =
+                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final WebView practiceWebView = binding.idWebViewPractice;
+        final ProgressBar loadingPB = binding.idPBLoading;
+        practiceWebView.loadUrl("https://practice.geeksforgeeks.org/");
+        practiceWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingPB.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                loadingPB.setVisibility(View.GONE);
+            }
+        });
+
+        practiceWebView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (keyEvent.getAction()==KeyEvent.ACTION_DOWN){
+                    switch (i){
+                        case KeyEvent.KEYCODE_BACK:
+                            if (practiceWebView.canGoBack()){
+                                practiceWebView.goBack();
+                            }
+                    }
+                }
+                return false;
+
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
